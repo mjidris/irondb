c9136f34c07cfcce78e0898f6c167117c709a4f2
JeffFallis <jeffreyfallis@hotmail.com>
2019-11-18 01:03:10 -0700

    Almost finished with index page. Missing group values

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,47 @@ function App() {
               <h1>Iron Meteorite Database</h1>
           </div>
       	</div>
+        <div className="container-fluid pt-3" id="bottom-container">
+          <div className="d-flex flex-row align-items-center justify-content-center mt-5 mb-2">
+              <h3>Start with a simple search:</h3>
+          </div>
+
+          <div className="d-flex flex-row align-items-center justify-content-center mb-4">
+            <form action="/database" method="POST" class="border border-dark p-3">
+              <div className="d-flex flex-row align-items-center justify-content-center ">
+                <div className="col-md-3">
+                  <label className="sr-only" for="name">Meteorite Name</label>
+                  <input type="text" name="name" id="name" class="form-control" placeholder="meteorite name" />
+                </div>
+                <div className="col-md-2">
+                  <label className="sr-only" for="group">group</label>
+                  <select className="form-control" name="group">
+                    <option value="group">group</option>
+                  </select>
+                </div>
+                <div className="col-md-2">
+                  <label className="sr-only" for="title">Paper Title</label>
+                  <input type="text" name="title" id="title" class="form-control" placeholder="paper title" />
+                </div>
+                <div className="col-md-2">
+                  <label className="sr-only" for="author">Author</label>
+                  <input type="text" name="author" id="author" class="form-control" placeholder="author" />
+                </div>
+                <div className="col-md-2 col-sm-3">
+                  <button className="btn btn-warning btn-block">Search</button>
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <div className="d-flex flex-row align-items-center justify-content-center mb-4">
+            <h3> - or - </h3>
+          </div>
+
+          <div className="d-flex flex-row align-items-center justify-content-center">
+            <a href="/database" class="btn btn-warning">Enter the Database</a>
+          </div>
+        </div>
       </body>
     </div>
   );
