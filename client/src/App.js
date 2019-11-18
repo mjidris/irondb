fcabf04966a98581b758095601577ff4a2170151
JeffFallis <jeffreyfallis@hotmail.com>
2019-11-18 00:55:29 -0700

    Added title to index

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,11 @@ function App() {
     <div className="App">
       <body>
         <div className="Navbar"><Navbar /></div>
+        <div className="container-fluid pt-3 pb-5" id="top-container">
+          <div className="d-flex flex-row align-items-center justify-content-center mt-5 mb-2">
+              <h1>Iron Meteorite Database</h1>
+          </div>
+      	</div>
       </body>
     </div>
   );
