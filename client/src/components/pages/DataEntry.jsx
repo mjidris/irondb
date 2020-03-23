17c20063b9d635dd190fc8836e6ca844122dc1e9
Jonathan Rasmussen <jrasmus4@asu.edu>
2020-03-22 19:03:48 -0600

    Finish merging with react-migration

--- client/src/components/pages/DataEntry.jsx
+++ client/src/components/pages/DataEntry.jsx
@@ -1,75 +1,71 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
-    BrowserRouter as Router,
-    Link,
-    Switch,
-    Route,
-    useRouteMatch
-} from 'react-router-dom';
-import DataEntryAlert from '../data-entry-components/DataEntryAlert';
-import DataEntryEditor from '../data-entry-components/DataEntryEditor';
-import DataEntryPDF from '../data-entry-components/DataEntryPDF'
-import '../styles/DataEntry.scss';
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route,
+  useRouteMatch
+} from "react-router-dom";
+import DataEntryAlert from "../data-entry-components/DataEntryAlert";
+import DataEntryEditor from "../data-entry-components/DataEntryEditor";
+import DataEntryPDF from "../data-entry-components/DataEntryPDF";
+import "../styles/DataEntry.scss";
 
 const DataEntry = () => {
+  let { path, url } = useRouteMatch();
 
-    let { path, url } = useRouteMatch();
+  const [[alert, alertType], setAlert] = useState(["Alert", "success"]);
 
-    const [[alert, alertType], setAlert] = useState(['Alert', 'success']);
+  const [meteoriteData, setData] = useState({});
 
-    const [meteoriteData, setData] = useState({});
+  useEffect(() => {
+    console.log("Calling editor endpoint...");
+    fetch("/api/data-entry/editor")
+      .then(res => res.json())
+      .then(resData => {
+        setData({
+          data: resData.data,
+          elements: resData.Elements,
+          techniques: resData.Technique
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
 
-    useEffect(() => {
-        console.log("Calling editor endpoint...");
-        fetch('/data-entry/editor')
-            .then(res => res.json())
-            .then(resData => {
-                setData({
-                    data: resData.data,
-                    elements: resData.Elements,
-                    techniques: resData.Technique
-                })
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }, []);
+  return (
+    <div className="data-entry">
+      <DataEntryAlert alert={alert} alertType={alertType} />
+      <div class="container-fluid pt-3" id="content"></div>
+      <div class="d-flex flex-row align-items-center justify-content-center mt-5 mb-2">
+        <h3>Select a data entry approach below:</h3>
+      </div>
 
-    return (
-        <div className="data-entry">
-            <DataEntryAlert alert={alert} alertType={alertType} />
-            <div class="container-fluid pt-3" id="content"></div>
-            <div class="d-flex flex-row align-items-center justify-content-center mt-5 mb-2">
-                <h3>Select a data entry approach below:</h3>
-            </div>
+      <div class="d-flex flex-row align-items-center justify-content-center pt-3">
+        <Link
+          to={`${url}/pdf`}
+          class="btn btn-danger btn-lg text-light"
+          id="tool"
+        >
+          With PDF
+        </Link>
+        <h3 class="mx-4 mb-2"> - or - </h3>
+        <Link to={`${url}/editor`} class="btn btn-danger btn-lg">
+          Without PDF
+        </Link>
+      </div>
 
-            <div class="d-flex flex-row align-items-center justify-content-center pt-3">
-                <Link
-                    to={`${url}/pdf`}
-                    class="btn btn-danger btn-lg text-light"
-                    id="tool"
-                >
-                    With PDF
-                </Link>
-                <h3 class="mx-4 mb-2"> - or - </h3>
-                <Link 
-                    to={`${url}/editor`}
-                    class="btn btn-danger btn-lg"
-                >
-                    Without PDF
-                </Link>
-            </div>
-
-            <Switch>
-                <Route path={`${path}/editor`}>
-                    <DataEntryEditor meteoriteData={meteoriteData} />
-                </Route>
-                <Route path={`${path}/pdf`}>
-                    <DataEntryPDF setAlert={setAlert} />
-                </Route>
-            </Switch>
-        </div>
-    )
-}
+      <Switch>
+        <Route path={`${path}/editor`}>
+          <DataEntryEditor meteoriteData={meteoriteData} />
+        </Route>
+        <Route path={`${path}/pdf`}>
+          <DataEntryPDF setAlert={setAlert} />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
 export default DataEntry;
