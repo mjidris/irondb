853fe32bafb8cc992b5b9c90376cb4ce09da6bc4
mjidris <m4idris@gmail.com>
2020-04-01 17:12:40 -0700

    Avoid mixing spaces & tabs to conform to PEP 8
    Updated file to avoid mixing spaces and tabs and stick to one in conformance with PEP 8 standards.

--- API/external/pdfScraper/nlp4attributes.py
+++ API/external/pdfScraper/nlp4attributes.py
@@ -66,85 +66,85 @@ j = json.loads(sys.argv[1])
 fileName = j['fileName']
 paper = fileName
 path = '/usr/app/public/temp/'
-page_num_title = 1 # shouldn't be global, make it local
-page_num_authors = 1 # shouldn't be global, make it local
+page_num_title = 1  # shouldn't be global, make it local
+page_num_authors = 1  # shouldn't be global, make it local
 
 nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger') # pos_tag dependency
-nltk.download('maxent_ne_chunker') # ne_chunk dependency
-nltk.download('words') # ne_chunk dependency
+nltk.download('averaged_perceptron_tagger')  # pos_tag dependency
+nltk.download('maxent_ne_chunker')  # ne_chunk dependency
+nltk.download('words')  # ne_chunk dependency
 
 
 # retrieves raw text from any given pdf
 def convert_pdf_to_txt(path, pageNo=0):
-    text = ""
-    rsrcmgr = PDFResourceManager()
-    retstr = io.StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    fp = open(path, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
+	text = ""
+	rsrcmgr = PDFResourceManager()
+	retstr = io.StringIO()
+	codec = 'utf-8'
+	laparams = LAParams()
+	device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+	fp = open(path, 'rb')
+	interpreter = PDFPageInterpreter(rsrcmgr, device)
 
-    for page in PDFPage.get_pages(fp, pagenos=[pageNo], check_extractable=True):
-        pageNo += 1
-        interpreter.process_page(page)
-        text = retstr.getvalue()
-    
-    fp.close()
-    device.close()
-    retstr.close()
+	for page in PDFPage.get_pages(fp, pagenos=[pageNo], check_extractable=True):
+		pageNo += 1
+		interpreter.process_page(page)
+		text = retstr.getvalue()
 
-    return text
+	fp.close()
+	device.close()
+	retstr.close()
+
+	return text
 
 
 # stages the relevant parts of the pdf using NLTK sentence tokenization
 def stage_text(txt):
-    try:
-        sentences = nltk.word_tokenize(txt)
-    except LookupError:
-        nltk.download('punkt')
-        nltk.download('averaged_perceptron_tagger') # pos_tag dependency
-        nltk.download('maxent_ne_chunker') # ne_chunk dependency
-        nltk.download('words') # ne_chunk dependency
-        print("\n")
-        print("                  ******************************************")
-        print("                  DEPENDENCIES DOWNLOADED. PLEASE RUN AGAIN")
-        print("                  ******************************************\n\n")
+	try:
+		sentences = nltk.word_tokenize(txt)
+	except LookupError:
+		nltk.download('punkt')
+		nltk.download('averaged_perceptron_tagger')  # pos_tag dependency
+		nltk.download('maxent_ne_chunker')  # ne_chunk dependency
+		nltk.download('words')  # ne_chunk dependency
+		print("\n")
+		print("                  ******************************************")
+		print("                  DEPENDENCIES DOWNLOADED. PLEASE RUN AGAIN")
+		print("                  ******************************************\n\n")
 
-    return sentences
+	return sentences
 
 
 # extracts ranked key phrases from any text between two tagwords using RAKE
 def keyword_extract(pdf_name, below=" ", above=" ", pageNo=0):
-    page = convert_pdf_to_txt(path + pdf_name, pageNo)
-    if below == " " and above == " ":
-        relevant_text = page
-    else:
-    	if below in page:
-    		relevant_text = page.split(below)[1]
-    	if above in page:
-    		relevant_text = page.split(above)[0]
+	page = convert_pdf_to_txt(path + pdf_name, pageNo)
+	if below == " " and above == " ":
+		relevant_text = page
+	else:
+		if below in page:
+			relevant_text = page.split(below)[1]
+		if above in page:
+			relevant_text = page.split(above)[0]
 
-    r = Rake(ranking_metric=Metric.WORD_FREQUENCY)
-    keywords = r.extract_keywords_from_text(relevant_text)
-    ranked_kywrds = r.get_ranked_phrases()
-    scored_kywrds = r.get_ranked_phrases_with_scores()
+	r = Rake(ranking_metric=Metric.WORD_FREQUENCY)
+	keywords = r.extract_keywords_from_text(relevant_text)
+	ranked_kywrds = r.get_ranked_phrases()
+	scored_kywrds = r.get_ranked_phrases_with_scores()
 
-    return scored_kywrds
+	return scored_kywrds
 
 
 # extracts truncated title from top of any page in the pdf using magic
 def truncated_title(pdf_name):
-    global page_num_title
-    random_page = convert_pdf_to_txt(path + pdf_name, page_num_title)
+	global page_num_title
+	random_page = convert_pdf_to_txt(path + pdf_name, page_num_title)
 
-    title_trunc = random_page.split('\n\n', 1)[0]
-    while (title_trunc.split()[0].isdigit()) or (('Table' in title_trunc) is True):
-        page_num_title += 1
-        title_trunc = truncated_title(pdf_name)
+	title_trunc = random_page.split('\n\n', 1)[0]
+	while (title_trunc.split()[0].isdigit()) or (('Table' in title_trunc) is True):
+		page_num_title += 1
+		title_trunc = truncated_title(pdf_name)
 
-    return title_trunc.replace('\n', "")
+	return title_trunc.replace('\n', "")
 
 
 # extracts full title from the first page of the pdf using RAKE and NLTK
@@ -164,14 +164,15 @@ def title_extract(pdf_name):
 	date = date_extract(pdf_name)
 
 	##########################################################################
-	#OPTIMIZE: Organize keywords list by most significant instead of frequency
+	# OPTIMIZE: Organize keywords list by most significant instead of frequency
 	##########################################################################
 	if len(keywords) > 0:
 		for tpl in keywords:
 			for kywrd in tpl[1].split():
 				for line in relevant_text.split('\n\n'):
-					if ((1 < len(line.split()) < 20) and ("www." not in line.lower()) and (date not in line) and (str(int(date)+1) not in line) and (str(int(date)-1) not in line)):
-						if any(kywrd.lower()==wrd.lower() for wrd in line.split()):
+					if ((1 < len(line.split()) < 20) and ("www." not in line.lower()) and (date not in line) and (
+							str(int(date) + 1) not in line) and (str(int(date) - 1) not in line)):
+						if any(kywrd.lower() == wrd.lower() for wrd in line.split()):
 							title_full = line.split("Alex")[0]  # dammit Alex
 							return title_full
 
@@ -186,18 +187,18 @@ def title_extract(pdf_name):
 
 # extracts truncated authors from top of any page in the pdf using the truncated title
 def truncated_authors(pdf_name):
-    global page_num_authors
-    random_page = convert_pdf_to_txt(path + pdf_name, page_num_authors)
+	global page_num_authors
+	random_page = convert_pdf_to_txt(path + pdf_name, page_num_authors)
 
-    authors_trunc = random_page.split('\n\n', 2)
-    while authors_trunc[0] in truncated_title(pdf_name):
-        page_num_authors += 1
-        authors_trunc = truncated_authors(pdf_name)
+	authors_trunc = random_page.split('\n\n', 2)
+	while authors_trunc[0] in truncated_title(pdf_name):
+		page_num_authors += 1
+		authors_trunc = truncated_authors(pdf_name)
 
-    if (authors_trunc[0].replace(" ", "")).isdigit():
-        return authors_trunc[1]
+	if (authors_trunc[0].replace(" ", "")).isdigit():
+		return authors_trunc[1]
 
-    return authors_trunc[0]
+	return authors_trunc[0]
 
 
 # extracts authors from the first page of the pdf using NLTK named entity recognition
@@ -217,33 +218,33 @@ def authors_extract(pdf_name):
 	authors = ""
 
 	###########################################################################
-	#OPTIMIZE: use recursion of author detection to check for multiline authors
+	# OPTIMIZE: use recursion of author detection to check for multiline authors
 	###########################################################################
 	for line in relevant_text.split('\n\n'):
 		for chunk in nerd:
 			if type(chunk) == nltk.tree.Tree:
 				if chunk.label() == 'PERSON' and authors_full == "":
 					english_author = " ".join([leaf[0] for leaf in chunk.leaves()])
-					if ((english_author in line) and (len(line.split()) > 1) 
-						and ("university" not in line.lower()) and ("institute" not in line.lower())):
+					if ((english_author in line) and (len(line.split()) > 1)
+							and ("university" not in line.lower()) and ("institute" not in line.lower())):
 						authors_full = line
 					if authors_full.endswith(","):
-						authors_full += relevant_text.split('\n\n')[line_index+1]
+						authors_full += relevant_text.split('\n\n')[line_index + 1]
 		if authors_full == "":
 			for word in tagged:
 				if word[0] != ".":
-					if ((word[0] not in nltk.corpus.words.words()) and (word[1] == 'NNP') 
-						and (word[0] in line) and (len(line.split()) > 1) 
-						and ("university" not in line.lower()) and ("institute" not in line.lower())):
+					if ((word[0] not in nltk.corpus.words.words()) and (word[1] == 'NNP')
+							and (word[0] in line) and (len(line.split()) > 1)
+							and ("university" not in line.lower()) and ("institute" not in line.lower())):
 						authors_full = line
 					if authors_full.endswith(","):
-						authors_full += relevant_text.split('\n\n')[line_index+1]
+						authors_full += relevant_text.split('\n\n')[line_index + 1]
 		line_index += 1
 
 	if authors_full != "":
 		for element in authors_full:
-			if (element.isdigit() or element == "*" or element == "∗"  
-				or element == "†" or element == "‡"):
+			if (element.isdigit() or element == "*" or element == "∗"
+					or element == "†" or element == "‡"):
 				authors_full = authors_full.replace(element, "")
 		authors_full = authors_full.replace(" and", ",")
 		authors_full = authors_full.replace(" ,", ",")
@@ -282,31 +283,31 @@ def authors_extract(pdf_name):
 
 # extracts journal source from the pdf text using tagwords
 def source_extract(pdf_name):
-    page = convert_pdf_to_txt(path + pdf_name)
-    relevant_text = page.split("Abstract")[0]
-    source = ""
-    journal_tagword = "journal"
-    volume_tagword = " vol "
-    issue_tagword = " no "
-    line_clean = ""
+	page = convert_pdf_to_txt(path + pdf_name)
+	relevant_text = page.split("Abstract")[0]
+	source = ""
+	journal_tagword = "journal"
+	volume_tagword = " vol "
+	issue_tagword = " no "
+	line_clean = ""
 
-    ######################################################################
-    #OPTIMIZE: Pull out journal names from online catalogue and find match
-    ######################################################################
-    for line in relevant_text.split('\n\n'):
-    	if (len(line.split()) > 1 and ((journal_tagword in line.lower()) 
-    		or (volume_tagword in line.lower().replace(".", "")) 
-    		or (issue_tagword in line.lower().replace(".", "")) 
-    		or ("acta" in line.lower()) or ("erde" in line.lower()))):
-    		for word in line.split():
-    			line_clean += word + " "
-    		source = line_clean
+	######################################################################
+	# OPTIMIZE: Pull out journal names from online catalogue and find match
+	######################################################################
+	for line in relevant_text.split('\n\n'):
+		if (len(line.split()) > 1 and ((journal_tagword in line.lower())
+		                               or (volume_tagword in line.lower().replace(".", ""))
+		                               or (issue_tagword in line.lower().replace(".", ""))
+		                               or ("acta" in line.lower()) or ("erde" in line.lower()))):
+			for word in line.split():
+				line_clean += word + " "
+			source = line_clean
 
-    if (("copyright" in source.lower()) and 
-        (volume_tagword not in source.lower().replace(".", "").split("copyright")[1])):
-        source = source.split("Copyright")[0]
+	if (("copyright" in source.lower()) and
+			(volume_tagword not in source.lower().replace(".", "").split("copyright")[1])):
+		source = source.split("Copyright")[0]
 
-    return source
+	return source
 
 
 def journal_extract(pdf_name):
@@ -324,9 +325,9 @@ def journal_extract(pdf_name):
 				if word.isalpha():
 					journal += word + " "
 
-		tagwords = [" vol ", " vol. ", " Vol ", " Vol. ", 
-					" no ", " no. ", " No ", " No. ", 
-					"published in", "Published in"]
+		tagwords = [" vol ", " vol. ", " Vol ", " Vol. ",
+		            " no ", " no. ", " No ", " No. ",
+		            "published in", "Published in"]
 		for tag in tagwords[0:8]:
 			journal = journal.split(tag)[0]
 		for tag in tagwords[8:10]:
@@ -341,72 +342,71 @@ def journal_extract(pdf_name):
 
 
 def volume_extract(pdf_name):
-    source = source_extract(pdf_name).replace(",", "").replace(".", "")
-    journal = journal_extract(pdf_name).replace(",", "").replace(".", "")
-    volume = ""
+	source = source_extract(pdf_name).replace(",", "").replace(".", "")
+	journal = journal_extract(pdf_name).replace(",", "").replace(".", "")
+	volume = ""
 
-    if len(source) > 0:
-        tagwords = [" vol ", " vol. ", " Vol ", " Vol. "]
-        for tag in tagwords:
-            if tag in source:
-                vol_regex = re.findall(r'%s(\d+)' % tag, source, re.IGNORECASE)
-                volume = vol_regex[0]
+	if len(source) > 0:
+		tagwords = [" vol ", " vol. ", " Vol ", " Vol. "]
+		for tag in tagwords:
+			if tag in source:
+				vol_regex = re.findall(r'%s(\d+)' % tag, source, re.IGNORECASE)
+				volume = vol_regex[0]
 
-    if (volume == "") and (len(journal) > 0) and (journal in source):
-        vol_num = source.split(journal)[1].split()[0]
-        if any(char.isdigit() for char in vol_num):
-            volume = vol_num
+	if (volume == "") and (len(journal) > 0) and (journal in source):
+		vol_num = source.split(journal)[1].split()[0]
+		if any(char.isdigit() for char in vol_num):
+			volume = vol_num
 
-    return volume
+	return volume
 
 
 def issue_extract(pdf_name):
-    source = source_extract(pdf_name).replace(",", "").replace(".", "")
-    issue = ""
+	source = source_extract(pdf_name).replace(",", "").replace(".", "")
+	issue = ""
 
-    if len(source) > 0:
-        tagwords = [" no ", " no. ", " No ", " No. "]
-        for tag in tagwords:
-            if tag in source:
-                vol_regex = re.findall(r'%s(\d+)' % tag, source, re.IGNORECASE)
-                if len(vol_regex) > 0:
-                	issue = vol_regex[0]
+	if len(source) > 0:
+		tagwords = [" no ", " no. ", " No ", " No. "]
+		for tag in tagwords:
+			if tag in source:
+				vol_regex = re.findall(r'%s(\d+)' % tag, source, re.IGNORECASE)
+				if len(vol_regex) > 0:
+					issue = vol_regex[0]
 
-    return issue
+	return issue
 
 
 # extracts publishing date from the pdf text using RegEx
 def date_extract(pdf_name):
-    page = convert_pdf_to_txt(path + pdf_name)
-    relevant_text = page.split("Abstract")[0].lower()
-    source = source_extract(pdf_name)
-    date = ""
+	page = convert_pdf_to_txt(path + pdf_name)
+	relevant_text = page.split("Abstract")[0].lower()
+	source = source_extract(pdf_name)
+	date = ""
 
-    if "publish" in relevant_text:
-        relevant_text = relevant_text.rsplit("publish", 1)[1]
-    elif "available" in relevant_text:
-        relevant_text = relevant_text.rsplit("available", 1)[1]
-    elif "accept" in relevant_text:
-        relevant_text = relevant_text.rsplit("accept", 1)[1]
+	if "publish" in relevant_text:
+		relevant_text = relevant_text.rsplit("publish", 1)[1]
+	elif "available" in relevant_text:
+		relevant_text = relevant_text.rsplit("available", 1)[1]
+	elif "accept" in relevant_text:
+		relevant_text = relevant_text.rsplit("accept", 1)[1]
 
-    date = re.search(r'[1-2][0-9]{3}', source)
-    if date != None:
-        while (int(date.group()) < 1665) or (int(date.group()) > 2222):
-            source = source.replace(date.group(), "")
-            date = re.search(r'[1-2][0-9]{3}', source)
-    else:
-        date = re.search(r'[1-2][0-9]{3}', relevant_text)
+	date = re.search(r'[1-2][0-9]{3}', source)
+	if date != None:
+		while (int(date.group()) < 1665) or (int(date.group()) > 2222):
+			source = source.replace(date.group(), "")
+			date = re.search(r'[1-2][0-9]{3}', source)
+	else:
+		date = re.search(r'[1-2][0-9]{3}', relevant_text)
 
-    if date != None:
-        date = date.group()
-    else:
-        date = ""
+	if date != None:
+		date = date.group()
+	else:
+		date = ""
 
-    return date
+	return date
 
 
-
-# papers = ['Choietal_GCA_1995.pdf', 'Kracheretal_GCA_1980.pdf', 'Litasov2018_Article_TraceElementCompositionAndClas.pdf', 
+# papers = ['Choietal_GCA_1995.pdf', 'Kracheretal_GCA_1980.pdf', 'Litasov2018_Article_TraceElementCompositionAndClas.pdf',
 # 			'Malvinetal_GCA_1984.pdf', 'Ruzicka2014.pdf', 'RuzickaandHutson2010.pdf', 
 # 			'ScottandWasson_GCA_1976.pdf', 'Wasson_2004.pdf', 'Wasson_2010.pdf', 
 # 			'Wasson_GCA_2017.pdf', 'Wasson_Icarus_1970.pdf', 'WassonandChoe_GCA_2009.pdf', 
@@ -422,8 +422,8 @@ def date_extract(pdf_name):
 # 	print(paper + " ISSUE: " + issue_extract(paper) + '\n')
 # 	print(paper + " DATE: " + date_extract(paper) + '\n')
 
-attributes = {'title': title_extract(paper), 'authors': authors_extract(paper), 
-			  'journal_name': journal_extract(paper), 'volume': volume_extract(paper), 
-              'issue': issue_extract(paper), 'date': date_extract(paper)}
+attributes = { 'title': title_extract(paper), 'authors': authors_extract(paper),
+               'journal_name': journal_extract(paper), 'volume': volume_extract(paper),
+               'issue': issue_extract(paper), 'date': date_extract(paper) }
 attributes_json = json.dumps(attributes)
 print(attributes_json)
