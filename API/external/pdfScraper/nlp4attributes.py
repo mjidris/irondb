04b070ca9194a0bb9f7c73cc4c3e7b3bc782f875
mjidris <m4idris@gmail.com>
2020-04-07 20:04:37 -0700

    Move nlp banner to the bottom of the file
    where it belongs...

--- API/external/pdfScraper/nlp4attributes.py
+++ API/external/pdfScraper/nlp4attributes.py
@@ -22,6 +22,42 @@ from attribute_extractor import journal_extract
 from attribute_extractor import title_extract
 from attribute_extractor import volume_extract
 
+# global variables
+path = os.path.abspath('pdfs') + '/'
+# j = json.loads(sys.argv[1])
+# fileName = j['fileName']
+# paper = fileName
+paper = sys.argv[1]
+# path = '/usr/app/public/temp/'
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')  # pos_tag dependency
+nltk.download('maxent_ne_chunker')  # ne_chunk dependency
+nltk.download('words')  # ne_chunk dependency
+
+# papers = ['Choietal_GCA_1995.pdf', 'Kracheretal_GCA_1980.pdf', 'Litasov2018_Article_TraceElementCompositionAndClas.pdf',
+# 			'Malvinetal_GCA_1984.pdf', 'Ruzicka2014.pdf', 'RuzickaandHutson2010.pdf', 
+# 			'ScottandWasson_GCA_1976.pdf', 'Wasson_2004.pdf', 'Wasson_2010.pdf', 
+# 			'Wasson_GCA_2017.pdf', 'Wasson_Icarus_1970.pdf', 'WassonandChoe_GCA_2009.pdf', 
+# 			'WassonandChoi_2003.pdf', 'WassonandKallemeyn_GCA_2002.pdf', 'WassonandKimberlin_GCA_1967.pdf', 
+# 			'WassonandOuyang 1990.pdf', 'WassonandRichardson_GCA_2011.pdf', 'WassonandRubinandHassanzadeh_1990.pdf', 
+# 			'WassonandSchaudy_Icarus_1971.pdf', 'Wassonetal_GCA_2007.pdf']
+# for paper in papers:
+# 	print()
+# 	print(paper + " TITLE: " + title_extract(paper) + '\n')
+# 	print(paper + " AUTHOR(S): " + authors_extract(paper) + '\n')
+# 	print(paper + " JOURNAL: " + journal_extract(paper) + '\n')
+# 	print(paper + " VOLUME: " + volume_extract(paper) + '\n')
+# 	print(paper + " ISSUE: " + issue_extract(paper) + '\n')
+# 	print(paper + " DATE: " + date_extract(paper) + '\n')
+
+attributes = {'title': title_extract(paper, path), 'authors': authors_extract(paper, path),
+              'journal_name': journal_extract(paper, path), 'volume': volume_extract(paper, path),
+              'issue': issue_extract(paper, path), 'date': date_extract(paper, path)}
+attributes_json = json.dumps(attributes)
+print(attributes_json)
+
+
 """
 ======================================================================================================================
 ======================================================================================================================
@@ -64,38 +100,3 @@ from attribute_extractor import volume_extract
 ======================================================================================================================
 ======================================================================================================================
 """
-
-# global variables
-path = os.path.abspath('pdfs') + '/'
-# j = json.loads(sys.argv[1])
-# fileName = j['fileName']
-# paper = fileName
-paper = sys.argv[1]
-# path = '/usr/app/public/temp/'
-
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')  # pos_tag dependency
-nltk.download('maxent_ne_chunker')  # ne_chunk dependency
-nltk.download('words')  # ne_chunk dependency
-
-# papers = ['Choietal_GCA_1995.pdf', 'Kracheretal_GCA_1980.pdf', 'Litasov2018_Article_TraceElementCompositionAndClas.pdf',
-# 			'Malvinetal_GCA_1984.pdf', 'Ruzicka2014.pdf', 'RuzickaandHutson2010.pdf', 
-# 			'ScottandWasson_GCA_1976.pdf', 'Wasson_2004.pdf', 'Wasson_2010.pdf', 
-# 			'Wasson_GCA_2017.pdf', 'Wasson_Icarus_1970.pdf', 'WassonandChoe_GCA_2009.pdf', 
-# 			'WassonandChoi_2003.pdf', 'WassonandKallemeyn_GCA_2002.pdf', 'WassonandKimberlin_GCA_1967.pdf', 
-# 			'WassonandOuyang 1990.pdf', 'WassonandRichardson_GCA_2011.pdf', 'WassonandRubinandHassanzadeh_1990.pdf', 
-# 			'WassonandSchaudy_Icarus_1971.pdf', 'Wassonetal_GCA_2007.pdf']
-# for paper in papers:
-# 	print()
-# 	print(paper + " TITLE: " + title_extract(paper) + '\n')
-# 	print(paper + " AUTHOR(S): " + authors_extract(paper) + '\n')
-# 	print(paper + " JOURNAL: " + journal_extract(paper) + '\n')
-# 	print(paper + " VOLUME: " + volume_extract(paper) + '\n')
-# 	print(paper + " ISSUE: " + issue_extract(paper) + '\n')
-# 	print(paper + " DATE: " + date_extract(paper) + '\n')
-
-attributes = {'title': title_extract(paper, path), 'authors': authors_extract(paper, path),
-              'journal_name': journal_extract(paper, path), 'volume': volume_extract(paper, path),
-              'issue': issue_extract(paper, path), 'date': date_extract(paper, path)}
-attributes_json = json.dumps(attributes)
-print(attributes_json)
